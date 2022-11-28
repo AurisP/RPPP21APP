4fd3e4738f37e675b08064bb32425cf79e4643c2
AurisP <92265826+AurisP@users.noreply.github.com>
2022-11-28 20:31:06 +0100

    Remove login

--- RPPP21APP/Data/Rppp21Context.cs
+++ RPPP21APP/Data/Rppp21Context.cs
@@ -77,7 +77,7 @@ public partial class Rppp21Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\;Data Source=fantom.fer.hr,3000;Initial Catalog=RPPP21;Persist Security Info=True;TrustServerCertificate=True;User ID=rppp21;Password=mekbuda-518");
+        => optionsBuilder.UseSqlServer("Server=.\\;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
