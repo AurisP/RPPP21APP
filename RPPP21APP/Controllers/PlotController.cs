28dd6df81e40765312384cd53bc88cd3cc94882a
AurisP <aurispraam@gmail.com>
2022-12-03 12:29:56 +0100

    Added Edit to plots

--- RPPP21APP/Controllers/PlotController.cs
+++ RPPP21APP/Controllers/PlotController.cs
@@ -32,11 +32,11 @@ namespace RPPP21APP.Controllers
 
         public async Task<IActionResult> Create()
         {
-            CreatePlotViewModel viewModel = new CreatePlotViewModel()
+            CreatePlotViewModel plotVM = new CreatePlotViewModel()
             {
                 WeatherConditions = await _weatherConditionsRepository.GetAll()
             };
-            return View(viewModel);
+            return View(plotVM);
         }
 
         [HttpPost]
@@ -56,7 +56,50 @@ namespace RPPP21APP.Controllers
                 await context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-               
+
         }
-    }   
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var entity = context.Plots.FirstOrDefault(item => item.PlotId == id);
+                if (entity == null) return View("Error");
+                var plotVM = new CreatePlotViewModel
+                {
+                    Name = entity.Name,
+                    Coordinates = entity.Coordinates,
+                    Area = entity.Area,
+                    WeatherConditionsId = entity.WeatherConditionsId,
+                    WeatherConditions = await _weatherConditionsRepository.GetAll()
+                };
+                return View(plotVM);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, CreatePlotViewModel plotVM)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var plot = new Plot
+                {
+                    Name = plotVM.Name,
+                    Coordinates = plotVM.Coordinates,
+                    Area = plotVM.Area,
+                    WeatherConditionsId = plotVM.WeatherConditionsId,
+                    PlotId = id
+                   
+                };
+                var entity = await context.Plots.FirstOrDefaultAsync(x => x.PlotId == id);
+                context.Plots.Update(entity);
+                //context.Entry(entity).Property(x => x.PlotId).IsModified = false;
+                context.Entry(entity).CurrentValues.SetValues(plot);                
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
 }
